53f6a29c4c084d359348592d49679a2d827f6d6e
OlgaStarchenko <o9915530@gmail.com>
2025-10-02 21:04:28 +1000

    Повесила обработчик событий на кнопки создание нового дела, удаление и выполнение дела

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const footerButton = document.querySelector(".footer__button");
 const modal = document.querySelector(".modal");
 const modalCancel = document.querySelector(".modal__cancel");
 const modalApply = document.querySelector(".modal__apply");
+const modalInput = document.querySelector(".modal__input");
 
 let todos = [
   {
@@ -29,18 +30,58 @@ let todos = [
 footerButton.addEventListener("click", () => {
   modal.classList.add("modal__open");
 });
-modalCancel.addEventListener("click", () => {
-  modal.classList.remove("modal__open");
-});
+modalCancel.addEventListener("click", closeModal);
 
 headerButton.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
 
+modalApply.addEventListener("click", () => {
+  const value = modalInput.value;
+  if (value.trim() === "") {
+    alert("Название задачи не может быть пустой");
+    return;
+  }
+  todos.push({
+    id: Date.now(),
+    text: value,
+    isComplete: false,
+  });
+  renderTodos(todos);
+  closeModal();
+});
+
 function renderTodos(list) {
+  main.innerHTML = null;
   list.forEach((task) => {
     const clone = todoTemplate.content.cloneNode(true);
+    const todoName = clone.querySelector(".todo__name");
+    const todoDelete = clone.querySelector(".todo__delete");
+    const todoEdit = clone.querySelector(".todo__edit");
+    const todoCheck = clone.querySelector(".todo__check");
+    todoDelete.addEventListener("click", () => deleteTodo(task));
+    todoCheck.addEventListener("click", () => completeTodo(task));
+    todoName.textContent = task.text;
+    if (task.isComplete) {
+      const todo = clone.querySelector(".todo");
+      todo.classList.add("todo__complete");
+    }
     main.append(clone);
   });
+}
+
+function closeModal() {
+  modal.classList.remove("modal__open");
+  modalInput.value = "";
+}
+
+function deleteTodo(task) {
+  todos = todos.filter((el) => el.id !== task.id);
+  renderTodos(todos);
+}
+
+function completeTodo(task) {
+  task.isComplete = !task.isComplete;
+  renderTodos(todos);
 }
 renderTodos(todos);
